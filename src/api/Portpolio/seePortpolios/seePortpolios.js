c45a76533037cc931d02b828f36aeb286be2f171
baekjung <zvgandam@gmail.com>
2019-10-29 15:48:50 +0900

    Archive Tag search add.

--- src/api/Portpolio/seePortpolios/seePortpolios.js
+++ src/api/Portpolio/seePortpolios/seePortpolios.js
@@ -1,4 +1,5 @@
 import Portpolio from "../../../models/Portpolio";
+import Tag from "../../../models/Tag";
 
 export default {
   Query: {
@@ -8,10 +9,23 @@ export default {
       let query = {};
 
       if (keyword) {
+        const tagData = await Tag.find({
+          value: { $regex: keyword, $options: "i" }
+        });
+
+        const tagIds = [];
+
+        if (tagData) {
+          for (let i = 0; i < tagData.length; i++) {
+            tagIds.push(tagData[i]._id);
+          }
+        }
+
         query = {
           $or: [
             { title: { $regex: keyword, $options: "i" } },
-            { description: { $regex: keyword, $options: "i" } }
+            { description: { $regex: keyword, $options: "i" } },
+            { tags: { $in: tagIds } }
           ]
         };
       }
