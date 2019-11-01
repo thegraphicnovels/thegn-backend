e3376394f46ec58feeff37617bcb0f35b263f290
baekjung <zvgandam@gmail.com>
2019-11-01 18:30:18 +0900

    modifyUser modify

--- src/api/User/modifyUser/modifyUser.js
+++ src/api/User/modifyUser/modifyUser.js
@@ -1,12 +1,31 @@
 import User from "../../../models/User";
+import { hashPassword } from "../../../utils";
 
 export default {
   Mutation: {
     modifyUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      const { id, name } = args;
+      const { user } = request;
+      const { id, password, name } = args;
 
-      return User.findOneAndUpdate({ id }, { name });
+      const oUser = await User.findOne({ id });
+
+      if (password) {
+        const hashedPassword = await hashPassword(password);
+        oUser.password = hashedPassword;
+      } else {
+        oUser.password = user.password;
+      }
+
+      if (name) {
+        oUser.name = name;
+      } else {
+        oUser.name = user.name;
+      }
+
+      oUser.save();
+
+      return oUser;
     }
   }
 };
